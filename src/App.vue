d58ee4ce95cf918a2e329ab90383ac6f0b5ac54c
Maureen-Ye <807524935@qq.com>
2021-02-05 12:41:55 +0800

    555

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    {{massage}}
+    <zheXian></zheXian>
+<div id="charts1"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import zheXian from './components/zheXian.vue'
+
+
+  export default {
+    components:{
+    HelloWorld,
+      zheXian,
+
+    },
+  name: 'App',
+  data(){
+    return{
+      massage:'hello world',
+
+    }
+  },
+
+    methods:{
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/bingTu.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "bingTu"
+    }
+</script>
+
+<style scoped>
+
+</style>
